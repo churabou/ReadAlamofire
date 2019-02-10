66467262376e6458fd6caefd7e2bc100d7a6f510
churabou <ginga7888@gmail.com>
2019-02-10 23:53:51 +0900

    init

---
+++ ReadAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadAlamofire
+//
+//  Created by chutatsu on 2019/02/10.
+//  Copyright © 2019 churabou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
